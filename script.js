16de1c06552d5fa5e54718f56d576a4a4ddaccdf
Akshat Panchal <AkshatPanchal0001@outlook.com>
2026-07-08 17:18:39 +0530

    Refactor button click event for stock calculations

--- script.js
+++ script.js
@@ -1,24 +1,89 @@
-document.getElementById("btn").addEventListener("click",(btn) =>{
+document.getElementById("btn").addEventListener("click", (btn) => {
     console.log(btn);
     console.log("pressed calculate");
 
-    const dte = document.getElementById("dt").value;
-    console.log(dte);
+    let mindte = new Date(document.getElementById("dt").value);
+    console.log(typeof mindte);
+    if (isNaN(mindte.getTime())) {
+        alert("Please Enter Date")
+    }
+
+    let maxdte = new Date(document.getElementById("dt").value);
+    console.log(typeof maxdte);
+    if (isNaN(mindte.getTime())) {
+        alert("Please Enter Date")
+    }
+
+    let pos = document.getElementById("ps").value;
+    if (pos === "") {
+        alert("Please Enter Prices of a Stock")
+    }
+    pos = pos.split(",");
+    pos = pos.map(Number)
+    console.log(typeof pos);
+
+    let qt = document.getElementById("qty").value;
+    if (qt === "") {
+        alert("Please Enter Quantity")
+    }
+    qt = Number(qt);
+    console.log(typeof qt);
+
+    let htn = document.getElementById("ht").value;
+    if (htn === "") {
+        alert("Please Enter Holding Time of Stock")
+    }
+    htn = Number(htn);
+    console.log(typeof htn);
+
+    if (pos.length < htn) {
+        alert("Holding Days can't be more then the no. of Prices are filled !")
+        return
+    }
+
+    let mind = 0;
+    let minp = pos[0];
+    for (let i = 0; i < pos.length; i++) {
+        if (pos[i] < minp) {
+            minp = pos[i]
+            mind = i
+        }
+    }
+    mindte.getDate();
+    mindte.setDate(mindte.getDate() + mind);
+
+    document.getElementById("minn").textContent = minp;
+    document.getElementById("minnd").textContent = mindte.toLocaleDateString();
 
 
-    const pos = document.getElementById("ps").value;
-    pos.split(",");
-    Number(pos);
-    console.log(typeof dte[0]);
-    console.log(pos);
+    let maxd;
+    let maxp = pos[0];
+    for (let g = 0; g < pos.length; g++) {
+        if (pos[g] > maxp) {
+            maxp = pos[g]
+            maxd = g
+        }
+    }
+    
+    maxdte.getDate();
+    maxdte.setDate(maxdte.getDate() + maxd);
+    document.getElementById("maxx").textContent = maxp;
+    document.getElementById("maxxd").textContent = maxdte.toLocaleDateString();
 
-    const qt = document.getElementById("qty").value;
-    Number(qt);
-    typeof Number;
-    console.log(qt);
+    for (let i = 1; i < pos.length; i++) {
+        let profit = (pos[i] - pos[0]) * qt;
+        let daynum = i + 1;
+        if (profit < 0) {
+            profit = 0;
+        }
+        document.getElementById("d" + daynum).textContent = profit;
+    }
 
-    const htn = document.getElementById("ht").value;
-    console.log(htn);
+    let netpl = (pos[htn - 1] - pos[0]) * qt;
+    document.getElementById("np").textContent = netpl;
+
 })
+
+
 
 
